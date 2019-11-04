c509c72ebf41c72072c3d916f21dcb4ac8ed191c
Sapir Elyovitch <sapirely@cs.huji.ac.il>
2019-11-04 11:47:28 +0200

    Unit 2, lesson 4.3, task 4

---
+++ app/src/main/java/com/unit2/dialogforalert/MainActivity.java
@@ -0,0 +1,47 @@
+package com.unit2.dialogforalert;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickShowAlert(View view) {
+        AlertDialog.Builder myAlertBuilder = new
+                AlertDialog.Builder(MainActivity.this);
+        // Set the dialog title and message.
+        myAlertBuilder.setTitle(getString(R.string.alert_title));
+        myAlertBuilder.setMessage(getString(R.string.alert_message));
+
+        // Add the dialog buttons
+        myAlertBuilder.setPositiveButton(getString(R.string.ok_button), new
+                DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // User clicked OK button.
+                        Toast.makeText(getApplicationContext(), "Pressed OK",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+        myAlertBuilder.setNegativeButton(getString(R.string.cancel_button), new
+                DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // User cancelled the dialog.
+                        Toast.makeText(getApplicationContext(), "Pressed Cancel",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        // Create and show the AlertDialog.
+        myAlertBuilder.show();
+    }
+}
